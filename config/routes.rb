6beb14a39fed32e3207951142f5841e89a957adf
il.zoff <il.zoff@gmail.com>
2010-10-03 17:00:47 +0400

    Done with Skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Aries::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => 'pages#about'
 
   # See how all your routes lay out with "rake routes"
 
