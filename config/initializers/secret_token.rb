e074618c924179c5278cffc32ac198af41a68f79
il.zoff <il.zoff@gmail.com>
2010-10-03 15:17:42 +0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aries::Application.config.secret_token = 'a8c0b71451c53a2abb20353faf9b2019fbe4f27be482d044e534cf5178805393cb786098b1c7e0b5f34fc7a6b5f7e1135419a2ecf2e915a8fe5db009b564f689'
